d2c0e2420d5e5ce97ac19cddddd53ccaa585626b
kavallir <152403875+kavallir@users.noreply.github.com>
2024-03-21 19:29:23 +0100

    Added date, npm icons and units changer

--- src/Weather.js
+++ src/Weather.js
@@ -1,75 +1,70 @@
 import React, { useState } from "react";
+
+import WeatherInfo from "./WeatherInfo";
+import axios from "axios";
 import "./Weather.css";
-import FormattedDate from "./FormattedDate";
-import axios from 'axios';
 
-export default function Weather (props) {
-
-    const [weatherData, setWeatherData] = useState({ ready: false });
-    const [city, setCity] = useState(props.defaultCity);
-
+export default function Weather(props) {
+  let [weatherData, setWeatherData] = useState({ ready: false });
+  let [city, setCity] = useState(props.defaultCity);
   function handleResponse(response) {
+    console.log(response.data);
     setWeatherData({
       ready: true,
-      coordinates: response.data.coord,
-      temperature: response.data.main.temp,
-      humidity: response.data.main.humidity,
-      date: new Date(response.data.dt * 1000),
-      description: response.data.weather[0].description,
-      icon: response.data.weather[0].icon,
+      city: response.data.city,
+      country:response.data.country,
+      date: new Date(response.data.time * 1000),
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
       wind: response.data.wind.speed,
-      city: response.data.name,
+      description: response.data.condition.description,
+      icon: response.data.condition.icon,
     });
   }
 
-    const apiKey = "0a521eaf234a3a56f45252fac3c737ad";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  function search() {
+    let apiKey = "9tf8b32027ebcbc4fod15cda0efbf2e3";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
-    if (weatherData.ready) {
-        return (
-    <div className="Weather">
-    <header>
-        <form className="search-form">
-            <div className="row">
-                <div className="col-9">
-                <input type="search" 
-            placeholder="Enter a city..." 
-            required 
-            className="form-control"
-            autoFocus="on" />
-                </div>
-                <div className="col-3">
-                <input type="submit" value="search" className="btn btn-primary w-100"/>
-                </div>
-                </div>
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
         </form>
-    </header>
-    <main>
-        <h1>{weatherData.city}</h1>
-        <ul>
-            <li>
-              <FormattedDate date={weatherData.date} />
-              </li>
-            <li className="text-capitalize">{weatherData.description}</li>
-        </ul>
-        <div className="row mt-3">
-            <div className="col-6"> 
-            <div className="clearfix">
-            <img src={weatherData.icon} alt="condition" className="float-left" />
-              <span className="temperature">{Math.round(weatherData.temperature)}</span>
-              <span className="unit">°C</span>
-              </div>
-              </div>
-            <div className="col-6">
-                <ul>
-                    <li>Humidity: {weatherData.humidity}%</li>
-                    <li>Wind: {weatherData.wind} km/h</li>
-                    </ul>
-                </div>
-        </div>
-</main>
-</div>
-); } else {
-    return "Loading...";
+        <WeatherInfo data={weatherData} />
+      </div>
+    );
+  } else {
+    search();
+    return "Loading";
   }
 }
